b56762c89ccd76f345386eff3798549afe5e6dca
zhoushisheng <1358281371@qq.com>
2018-10-12 22:32:30 +0800

    121

--- src/main/java/com/zhuguan/zhou/Application.java
+++ src/main/java/com/zhuguan/zhou/Application.java
@@ -11,5 +11,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("OK");
+        System.out.println("haoliao");
     }
 }
