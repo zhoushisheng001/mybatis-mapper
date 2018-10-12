03c6e248796ace503c24a0f0c557a1d6d7029547
zhoushisheng <1358281371@qq.com>
2018-10-12 22:49:13 +0800

    map

--- src/main/java/com/zhuguan/zhou/Application.java
+++ src/main/java/com/zhuguan/zhou/Application.java
@@ -4,13 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 //@ComponentScan("com.zhuguan.zhou.mapper")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("OK");
         System.out.println("haoliao");
+        Map<String, String> paraMap = new HashMap<>();
+        paraMap.put("121","江西");
     }
 }
